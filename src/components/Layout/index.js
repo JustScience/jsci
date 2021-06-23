3a44812c5ef392dd84e9f27a0730586342171cfa
J Galenti <thejustscience@gmail.com>
2021-06-22 21:39:42 -0600

    Move Global.css import to Layout component - FIX PATH

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,7 +4,7 @@ import Footer from '../Footer'
 // import Responsive from '../Responsive'
 import LayoutWrap from './style'
 
-import './../styles/global.css'
+import '../../styles/global.css'
 
 export default function Layout({ children }) {
     return (
