e3082e58b5db49619fafc42a417f8d9f20786250
J Galenti <thejustscience@gmail.com>
2021-06-24 20:31:52 -0600

    Add General Structured Data

--- src/components/SEO/SearchEngine.js
+++ src/components/SEO/SearchEngine.js
@@ -41,6 +41,83 @@ export default function SearchEngine({title, description, metaImage, url}) {
       <meta name="twitter:title" content={title || site.siteMetadata.title} />
       <meta name="twitter:description" content={description || site.siteMetadata.description} />
       <meta name="twitter:image" content={metaImage || defaultMetaImage} />
+
+      <script type="application/ld+json">
+        {`
+          {
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "url": "https://jsci.io",
+            "name": "JSci Beats",
+            "contactPoint": {
+              "@type": "ContactPoint",
+              "email": "jscibeats@gmail.com",
+              "contactType": "Business Inquiries"
+            },
+            "hasOfferCatalog": {
+              "@type": "OfferCatalog",
+              "name": "Melody Loop Samples",
+              "itemListElement": [
+                {
+                  "@type": "OfferCatalog",
+                  "name": "Music Composition Licenses",
+                  "itemListElement": [
+                    {
+                      "@type": "Offer",
+                      "itemOffered": {
+                        "@type": "Service",
+                        "name": "24 bit, 44.1k Stereo WAV Audio"
+                      }
+                    },
+                    {
+                      "@type": "Offer",
+                      "itemOffered": {
+                        "@type": "Service",
+                        "name": "Full Quality Instrument Mix Stems"
+                      }
+                    },
+                    {
+                      "@type": "Offer",
+                      "itemOffered": {
+                        "@type": "Service",
+                        "name": "Original MIDI Compositions"
+                      }
+                    }
+                  ]
+                },
+                {
+                  "@type": "OfferCatalog",
+                  "name": "Music Sync Licenses",
+                  "itemListElement": [
+                    {
+                      "@type": "Offer",
+                      "itemOffered": {
+                        "@type": "Service",
+                        "name": "Web Content Creator Music"
+                      }
+                    },
+                    {
+                      "@type": "Offer",
+                      "itemOffered": {
+                        "@type": "Service",
+                        "name": "Film, TV & Video Game Music"
+                      }
+                    },
+                    {
+                      "@type": "Offer",
+                      "itemOffered": {
+                        "@type": "Service",
+                        "name": "Advertising Music for Commercials"
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        `}
+      </script>
     </Helmet>
+    
   )
 }
